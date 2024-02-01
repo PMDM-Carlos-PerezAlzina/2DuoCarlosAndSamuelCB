16f3c5e72bee7c1f26f408d62d6f99a70dfc6446
Carlos Pérez Alzina <carlosperezalzina@gmail.com>
2024-02-01 21:45:25 +0100

    Subiendo0 cambios para trabajar en windows

--- Assets/Scripts/EnemiesScripts/BurningGhoulController.cs
+++ Assets/Scripts/EnemiesScripts/BurningGhoulController.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 public class BurningGhoulController : EnemyInterface
 {
     public GameObject deathObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb2d;
+    [SerializeField] private float moveSpeed = 2.0f;
+    public LayerMask down;
+    public LayerMask front;
+    public float distanceDown;
+    public float distanceFront;
+    public Transform downController;
+    public Transform frontController;
+    public bool downInformation;
+    public bool frontInformation;
+    private bool isLookingRight = true;
 
     // Update is called once per frame
     void Update()
     {
-        if (base.life <= 0) {
+        if (life <= 0)
+        {
             base.Die(gameObject, deathObject);
         }
+
+        rb2d.velocity = new Vector3(moveSpeed, rb2d.velocity.y);
+
+        frontInformation = Physics2D.Raycast(frontController.position, transform.right, distanceFront, front);
+        downInformation = Physics2D.Raycast(downController.position, transform.up * -1, distanceDown, down);
+
+        if (frontInformation || !downInformation) {
+            Flip();
+        }
+    }
+
+    private void Flip() {
+        isLookingRight = !isLookingRight;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+        moveSpeed = moveSpeed * -1;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(downController.transform.position, downController.transform.position + transform.up * -1 * distanceDown);
+        Gizmos.DrawLine(frontController.transform.position, frontController.transform.position + transform.right * distanceFront);
     }
 }
--- Assets/Scripts/EnemiesScripts/GhostController.cs
+++ Assets/Scripts/EnemiesScripts/GhostController.cs
@@ -17,9 +17,6 @@ public class GhostController : EnemyInterface
     public bool frontInformation;
     private bool isLookingRight = true;
 
-    public float followDistance = 5.0f;  // Definir la distancia a la que el ghost empezará a seguir al jugador
-    public Transform player;  // Referencia al objeto del jugador
-
     // Update is called once per frame
     void Update()
     {
@@ -28,48 +25,23 @@ public class GhostController : EnemyInterface
             base.Die(gameObject, deathObject);
         }
 
-        // Calcular la distancia entre el ghost y el jugador
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        rb2d.velocity = new Vector3(moveSpeed, rb2d.velocity.y);
 
-        // Si la distancia es menor que la distancia de seguimiento, seguir al jugador
-        if (distanceToPlayer < followDistance)
-        {
-            // Calcular la dirección hacia el jugador
-            float direction = Mathf.Sign(player.position.x - transform.position.x);
+        frontInformation = Physics2D.Raycast(frontController.position, transform.right, distanceFront, front);
+        downInformation = Physics2D.Raycast(downController.position, transform.up * -1, distanceDown, down);
 
-            // Mover el ghost hacia el jugador
-            rb2d.velocity = new Vector2(moveSpeed * direction, rb2d.velocity.y);
-
-            // Voltear el ghost según la dirección hacia el jugador
-            if ((direction > 0 && !isLookingRight) || (direction < 0 && isLookingRight))
-            {
-                Flip();
-            }
-        }
-        else
-        {
-            // Si el jugador está fuera de la distancia de seguimiento, continuar moviéndose en la dirección predeterminada
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-
-            frontInformation = Physics2D.Raycast(frontController.position, transform.right, distanceFront, front);
-            downInformation = Physics2D.Raycast(downController.position, transform.up * -1, distanceDown, down);
-
-            if (frontInformation || !downInformation)
-            {
-                Flip();
-            }
+        if (frontInformation || !downInformation) {
+            Flip();
         }
     }
 
-    private void Flip()
-    {
+    private void Flip() {
         isLookingRight = !isLookingRight;
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
         moveSpeed = moveSpeed * -1;
     }
 
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(downController.transform.position, downController.transform.position + transform.up * -1 * distanceDown);
         Gizmos.DrawLine(frontController.transform.position, frontController.transform.position + transform.right * distanceFront);
